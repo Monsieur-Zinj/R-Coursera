d19592bb02d0403c5c9395c7c7d7c079b69846f0
Mr-Zinj <ludwig.trossel@gmail.com>
2022-04-06 22:14:46 +0200

    Commits on Apr 6, 2022
    Data Science: Foundation using R
    
    C2 R programming
    S2 - PA  : first 2nd part done
    # data frame (data.frame)
    # list, unlist

--- Data Science: Foundations using R/C2 - R programming/S2 - PA.R
+++ Data Science: Foundations using R/C2 - R programming/S2 - PA.R
@@ -1,24 +1,37 @@
-path="~/Documents/R-Coursera/R-Coursera/Data Science: Foundations using R/C2 - R programming/specdata/"
-library("eList")
+library(comprehenr)
 
-variable<-"nitrate"
-ex <- paste(c(1,10,20,320)) # convert to string
+# create a list of dataframe depending on id provided
+make_list_data <- function(directory, id){
+  path="~/Documents/R-Coursera/R-Coursera/Data Science: Foundations using R/C2 - R programming/"
+  path=paste(path,directory,sep="")
+
+  id_char<-paste(id) # num to char
+
+  zeros<-to_vec(for (i in nchar(id_char)) paste(rep("0",3-i),collapse = "")) # calculate how much zeros need to be padded before file name 
+  
+  file_list <- paste0(path,"/", zeros,id_char,".csv") #concatenate path,zeros, number and .csv
+  
+  myfiles = lapply(file_list, read.csv) # read all file and put data in a list
+  
+  myfiles
+
+}
+
 
 
 pollutantmean <- function(directory, polluant, id=1:332){
   
+
   # return sum of variables and number of no na
   mean_and_pond <- function(file){
-    var_sum <- sum(file[,polluant], na.rm=TRUE)
-    no_na_nb <- sum(!is.na(file[,polluant]))
-    c(var_sum, no_na_nb)
+    
+      var_sum <- sum(file[,polluant], na.rm=TRUE)
+      no_na_nb <- sum(!is.na(file[,polluant]))
+      c(var_sum, no_na_nb)
   }
+  
+  myfiles<-make_list_data(directory,id)
 
-  id<-nchar(paste(id))
-  print(id)
-  zeros<-..[for (i in id) paste(rep("0",3-i),collapse = "")] # calculate how much zeros need to be padded before file name 
-  file_list <- paste0(directory,zeros,id,".csv") #concatenate path,zeros, number and .csv
-  myfiles = lapply(file_list, read.csv) # read all file and put data in a list
   
   mean_and_pond_res<-lapply(myfiles,mean_and_pond)
   mean_and_pond_res<-rowSums(matrix(unlist(mean_and_pond_res),nrow = 2)) # add all sum and no na nb
@@ -28,6 +41,28 @@ pollutantmean <- function(directory, polluant, id=1:332){
   
 }
 
+
+complete <- function(directory, id=1:332){
+  
+  myfiles<-make_list_data(directory,id)
+  
+  complete_one_dataframe <- function(file){
+  
+      complete_nb<-sum(rowSums(!is.na(file[,c(2,3)]), na.rm = TRUE)==2)  
+      complete_nb  
+  }
+  
+  complete_vect<- unlist(lapply(myfiles,complete_one_dataframe))
+  
+  var<-list(id,complete_vect)
+  var<-data.frame(var)
+  colnames(var)<-c("id","nobs")
+  var
+}
+  
+  
+  
+  
 
 
 
